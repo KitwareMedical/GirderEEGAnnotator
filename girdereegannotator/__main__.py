77fe48d1b4aee50c605413c4d738f10bda7b9d79
Justine Antoine <justine.antoine@kitware.com>
2026-06-19 18:01:45 +0200

    chore: integrate database portal to replace file browser

--- girdereegannotator/__main__.py
+++ girdereegannotator/__main__.py
@@ -1,8 +1,13 @@
+from trame_server.core import Server
+
 from girdereegannotator.core import AnnotatorApp
+from girdereegannotator.database.database_config import load_config, get_interface
 
 
-def main(**kwargs) -> None:
-    app = AnnotatorApp()
+def main(server: Server | None = None, **kwargs) -> None:
+    config = load_config(path="config.yaml")
+    interface = get_interface(config)
+    app = AnnotatorApp(interface=interface, server=server)
     app.server.start(**kwargs)
 
 
--- girdereegannotator/file_browser/__init__.py
+++
@@ -1,4 +0,0 @@
-from .file_browser_logic import FileBrowserLogic
-from .file_browser_ui import FileBrowserUI
-
-__all__ = ["FileBrowserLogic", "FileBrowserUI"]
