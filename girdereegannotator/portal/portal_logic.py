aa490b1f43e2d24d651a678aba9277dec7c2d68d
Justine Antoine <justine.antoine@kitware.com>
2026-06-19 18:01:05 +0200

    feat(portal): add database portal to select data

---
+++ girdereegannotator/portal/portal_logic.py
@@ -0,0 +1,99 @@
+import tempfile
+from pathlib import Path
+from typing import Any
+
+from numpy import median
+from trame_server import Server
+from trame_server.utils.typed_state import TypedState
+from undo_stack import Signal
+
+from girdereegannotator.database.models import EEGMedia, EEGMediaFile
+from girdereegannotator.portal.loader_logic import LoaderLogic
+from .portal_ui import PortalState, PortalUI
+
+ANNOTATION_FILE_SUFFIX = "annotations.csv"
+EEG_FILE_EXTENSIONS = (".neonatal", ".edf")
+
+
+class FileValidationError(Exception):
+    pass
+
+
+class AnnotatorLoadingError(Exception):
+    pass
+
+
+
+def are_eeg_files(files: list[EEGMediaFile]) -> bool:
+    return len(files) <= 2 and any(file.name.endswith(EEG_FILE_EXTENSIONS) for file in files)
+
+
+
+class PortalLogic:
+    eeg_media_loaded = Signal(str)
+
+    def __init__(self, server: Server) -> None:
+        self.server = server
+        self.typed_state = TypedState(self.server.state, PortalState)
+        self._current_eeg_media = self.typed_state.get_sub_state(self.typed_state.name.eeg_media)
+
+        self._loader_logic = LoaderLogic(server)
+        self._loader_logic.eeg_media_loaded.connect(self.eeg_media_loaded)
+
+        self.server.controller.on_server_ready.add(self._initialize)
+
+    @property
+    def name(self) -> PortalState:
+        return self.typed_state.name
+
+    @property
+    def data(self) -> PortalState:
+        return self.typed_state.data
+    
+    def _get_eeg_media_index(self) -> int:
+        return next((i for i, item in enumerate(self.data.eeg_media_list) if item._id == self.data.eeg_media._id), None)
+    
+    def _initialize(self, **_kwargs) -> None:
+        self.data.eeg_media_list = self.server.controller.list_eeg_media()
+
+    def set_ui(self, ui: PortalUI) -> None:
+        ui.eeg_media_selected.connect(self._select_eeg_media)
+        ui.save_annotations_clicked.connect(self._loader_logic.save_eeg_annotations)
+        ui.previous_eeg_clicked.connect(self._select_previous_media)
+        ui.next_eeg_clicked.connect(self._select_next_media)
+        # ui.approve_annotation_clicked.connect(self._approve_annotations)
+
+    def _select_eeg_media(self, eeg_media: EEGMedia) -> None:
+        self._current_eeg_media.set_dataclass(eeg_media)
+        #TODO async load
+        try:
+            self._loader_logic.load_eeg_media_files(eeg_media._id)
+        except Exception as e:
+            self._current_eeg_media.set_dataclass(EEGMedia())
+
+
+    def _select_next_media(self):
+        if len(self.data.eeg_media_list) == 0:
+            return
+
+        eeg_media_index = self._get_eeg_media_index()
+        if eeg_media_index == (len(self.data.eeg_media_list) - 1):
+            next_index = 0
+        elif eeg_media_index is None:
+            next_index = 0
+        else:
+            next_index = eeg_media_index + 1
+        self._select_eeg_media(self.data.eeg_media_list[next_index])
+
+    def _select_previous_media(self):
+        if len(self.data.eeg_media_list) == 0:
+            return
+        
+        eeg_media_index = self._get_eeg_media_index()
+        if eeg_media_index == 0:
+            previous_index = len(self.data.eeg_media_list) - 1
+        elif eeg_media_index is None:
+            previous_index = 0
+        else:
+            previous_index = eeg_media_index - 1
+        self._select_eeg_media(self.data.eeg_media_list[previous_index])
---
+++ girdereegannotator/portal/portal_ui.py
@@ -0,0 +1,99 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+from trame_dataclass.v2 import StateDataModel
+from trame_server.core import Server
+from trame.widgets import html
+from trame.widgets import vuetify3 as v3
+from trame_server.utils.typed_state import TypedState
+from undo_stack import Signal
+
+from girdereegannotator.database.models import EEGMedia, EEGMediaFile
+
+
+@dataclass
+class LoaderState:
+    eeg_loading: bool = False
+    eeg_file: EEGMediaFile = field(default_factory=EEGMediaFile)
+    eeg_annotation_file: EEGMediaFile = field(default_factory=EEGMediaFile)
+
+
+@dataclass
+class PortalState:
+    eeg_media: EEGMedia = field(default_factory=EEGMedia)
+    eeg_media_list: list[EEGMedia] = field(default_factory=list)
+
+
+class PortalUI:
+    eeg_media_selected = Signal(EEGMedia)
+    save_annotations_clicked = Signal()
+    approve_annotations_clicked = Signal()
+    previous_eeg_clicked = Signal()
+    next_eeg_clicked = Signal()
+
+    def __init__(self, server: Server):
+        self.server = server
+        self.portal_state = TypedState(server.state, PortalState)
+        self.loader_state = TypedState(server.state, LoaderState)
+
+    def _build_icon_button(self, icon: str, tooltip: str | None = None, **kwargs) -> None:
+        with v3.VBtn(**kwargs):
+            if tooltip is not None:
+                v3.VTooltip(
+                    text=tooltip,
+                    activator="parent",
+                    transition="slide-y-transition",
+                    location="bottom start",
+                )
+            v3.VIcon(icon=icon)
+
+    def _select_eeg_media(self, eeg_media_dict: dict[str, Any]) -> None:
+        eeg_media = EEGMedia(**eeg_media_dict)
+        self.eeg_media_selected(eeg_media)
+
+    def build_bar(self) -> None:
+        self._build_icon_button(
+            icon="mdi-content-save-outline",
+            click=self.save_annotations_clicked,
+            tooltip="Save annotations",
+            disabled=(f"!{self.portal_state.name.eeg_media.name}",),
+
+        )
+        v3.VSpacer()
+        self._build_icon_button(
+            icon="mdi-chevron-left",
+            click=self.previous_eeg_clicked,
+            tooltip="Previous EEG",
+
+        )
+        html.Div("{{ " +self.portal_state.name.eeg_media.name + " }}", classes="px-2")
+        self._build_icon_button(
+            icon="mdi-chevron-right",
+            click=self.next_eeg_clicked,
+            tooltip="Next EEG",
+        )
+        v3.VSpacer()
+
+
+    def build_drawer(self) -> None:
+        with (
+            v3.VList(),
+            v3.VListItem(
+                v_for=f"eeg_media in {self.portal_state.name.eeg_media_list}",
+                active=(f"{self.portal_state.name.eeg_media._id} === eeg_media._id",),
+                value=("eeg_media",),
+                title=("eeg_media.name",),
+                # Make sure eeg viewer takes the focus after loading
+                click=(
+                    "trigger('"
+                    f"{self.server.controller.trigger_name(self._select_eeg_media)}"
+                    "', [eeg_media]).then(() => { trame.refs.eegview.$refs.rootElem.focus(); })"
+                )
+            ),
+            v3.Template(v_slot_append="{ isActive }")
+        ):
+            v3.VProgressCircular(
+                v_if=(f"isActive && {self.loader_state.name.eeg_loading}",),
+                indeterminate=True,
+                size=15,
+            )
