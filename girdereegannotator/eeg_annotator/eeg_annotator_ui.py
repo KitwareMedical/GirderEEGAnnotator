88fb56187704485972c6677f6ed089a3188e28a4
Justine Antoine <justine.antoine@kitware.com>
2026-06-19 18:00:07 +0200

    fix(annotator): remove layout and add ref

--- girdereegannotator/eeg_annotator/eeg_annotator_ui.py
+++ girdereegannotator/eeg_annotator/eeg_annotator_ui.py
@@ -1,14 +1,17 @@
 from trame.ui.vuetify3 import VAppLayout
-from trame.widgets import rca
+from trame.widgets import html, rca
 from trame.widgets.vuetify3 import VMain
 from trame_server import Server
 
 from .eeg_annotator_shortcuts_panel import ShortcutsPanel
 
 
-class EGGAnnotatorUI:
-    def __init__(self, server: Server):
-        self.template_name = "eeg-annotator"
-        with VAppLayout(server, template_name=self.template_name), VMain(classes="d-flex flex-column pb-2"):
-            self.rca = rca.RemoteControlledArea(name="eeg-view", display="image", send_mouse_move=True)
+class EGGAnnotatorUI(html.Div):
+    def __init__(self) -> None:
+        super().__init__(classes="d-flex flex-column pb-2 fill-height")
+        self._build_ui()
+
+    def _build_ui(self) -> None:
+        with self:
+            self.rca = rca.RemoteControlledArea(ref="eegview", send_mouse_move=True)
             ShortcutsPanel()
