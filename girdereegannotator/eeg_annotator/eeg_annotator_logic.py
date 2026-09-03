5ca0fe5c330c7e5d8ecb8cd5ccaabd9eb033a7e3
Justine Antoine <justine.antoine@kitware.com>
2026-09-03 11:26:35 +0200

    feat(async save): make save annotations asynchronous

--- girdereegannotator/eeg_annotator/eeg_annotator_logic.py
+++ girdereegannotator/eeg_annotator/eeg_annotator_logic.py
@@ -12,8 +12,7 @@ from girdereegannotator.database.models import (
 )
 from girdereegannotator.utils.base_logic import BaseLogic
 
-from .eeg_annotator_state import EEGAnnotatorMode, EEGAnnotatorState
-from .eeg_annotator_ui import EGGAnnotatorUI
+from .eeg_annotator_ui import EEGAnnotatorMode, EEGAnnotatorState, EGGAnnotatorUI
 from .eeg_viewer_logic import EEGViewerLogic
 
 
@@ -25,47 +24,49 @@ class EGGAnnotatorLogic(BaseLogic[EEGAnnotatorState]):
     def __init__(self, server: Server):
         super().__init__(server, EEGAnnotatorState)
 
-        self.eeg_fileset = self.typed_state.get_sub_state(self.name.eeg_fileset)
-        self.annotations_file = self.typed_state.get_sub_state(self.name.annotations_file)
-        self.current_user = TypedState(self.state, User)
+        self._eeg_fileset_state = self.typed_state.get_sub_state(self.name.eeg_fileset)
+        self._annotations_file_state = self.typed_state.get_sub_state(self.name.annotations_file)
+        self._user_state = TypedState(self.state, User)
         self._viewer_logic = EEGViewerLogic(server)
 
-    def _upsert_annotations_file(self, new_annotations_file: AnnotationsFile) -> None:
-        if any(ann._id == new_annotations_file._id for ann in self.data.eeg_fileset.annotations_files):
-            self.data.eeg_fileset.annotations_files = [
-                new_annotations_file if ann._id == new_annotations_file._id else ann
-                for ann in self.data.eeg_fileset.annotations_files
-            ]
-        self.data.eeg_fileset.annotations_files = [*self.data.eeg_fileset.annotations_files, new_annotations_file]
+        self.bind_changes({self.name.eeg_fileset: self._on_eeg_fileset_updated})
 
-    def _refresh_eeg_fileset(self) -> None:
-        eeg_fileset = self.eeg_fileset.get_dataclass()
-        refreshed_eeg_fileset = self.ctrl.refresh_eeg_fileset(eeg_fileset)
-        self.eeg_fileset.set_dataclass(refreshed_eeg_fileset)
+    @property
+    def eeg_fileset(self) -> EEGFileset:
+        return self._eeg_fileset_state.get_dataclass()
 
-        self._refresh_annotator_mode()
+    @eeg_fileset.setter
+    def eeg_fileset(self, value: EEGFileset) -> None:
+        self._eeg_fileset_state.set_dataclass(value)
 
-    def _refresh_annotator_mode(self, *_args) -> None:
-        updated_eeg_fileset = self.eeg_fileset.get_dataclass()
-        updated_annotations_file = self.annotations_file.get_dataclass()
+    @property
+    def annotations_file(self) -> AnnotationsFile:
+        return self._annotations_file_state.get_dataclass()
 
+    @annotations_file.setter
+    def annotations_file(self, value: AnnotationsFile) -> None:
+        self._annotations_file_state.set_dataclass(value)
+
+    def _on_eeg_fileset_updated(self, *_args) -> None:
+        if self.eeg_fileset._id is not None:
+            self.eeg_fileset_updated(self.eeg_fileset)
+
+    def _refresh_annotator_mode(self) -> None:
         mode = EEGAnnotatorMode.UNDEFINED
 
-        if updated_eeg_fileset._id is not None:
-            self.eeg_fileset_updated(updated_eeg_fileset)
-
-            if updated_eeg_fileset.is_validated:
+        if self.eeg_fileset._id is not None:
+            if self.eeg_fileset.is_validated:
                 mode = EEGAnnotatorMode.DONE
 
-            elif updated_annotations_file._id is None or (
-                updated_annotations_file.status == AnnotationStatus.IN_PROGRESS
-                and updated_annotations_file.author._id == self.current_user.data._id
+            elif self.annotations_file._id is None or (
+                self.annotations_file.status == AnnotationStatus.IN_PROGRESS
+                and self.annotations_file.author._id == self._user_state.data._id
             ):
                 mode = EEGAnnotatorMode.ANNOTATE
 
             elif (
-                updated_annotations_file.status == AnnotationStatus.IN_REVIEW
-                and updated_annotations_file.author._id != self.current_user.data._id
+                self.annotations_file.status == AnnotationStatus.IN_REVIEW
+                and self.annotations_file.author._id != self._user_state.data._id
             ):
                 mode = EEGAnnotatorMode.REVIEW
 
@@ -74,31 +75,29 @@ class EGGAnnotatorLogic(BaseLogic[EEGAnnotatorState]):
 
         self.data.mode = mode
 
-    def _on_load_task_finished(self, task: Task) -> None:
+    def _on_task_finished(self, task: Task) -> None:
         eeg_fileset, annotations_file = task.result()
-        self.eeg_fileset.set_dataclass(eeg_fileset)
-        self.annotations_file.set_dataclass(annotations_file if annotations_file is not None else AnnotationsFile())
+        self.eeg_fileset = eeg_fileset
+        self.annotations_file = annotations_file if annotations_file is not None else AnnotationsFile()
         self._refresh_annotator_mode()
         self.state.flush()
 
     def _on_annotations_file_selected(self, annotations_file: AnnotationsFile | None = None) -> None:
-        self.load_eeg_fileset(self.eeg_fileset.get_dataclass(), annotations_file)
+        self.load_eeg_fileset(self.eeg_fileset, annotations_file)
 
     def load_eeg_fileset(self, eeg_fileset: EEGFileset | None, annotations_file: AnnotationsFile | None) -> None:
         if eeg_fileset is None:
             self.reset_state()
             return
 
-        is_new_eeg_fileset = self.eeg_fileset.data._id != eeg_fileset._id
+        is_new_eeg_fileset = self.eeg_fileset._id != eeg_fileset._id
 
         load_task = self._viewer_logic.load_eeg_files(eeg_fileset, annotations_file, is_new_eeg_fileset)
-        load_task.add_done_callback(self._on_load_task_finished)
+        load_task.add_done_callback(self._on_task_finished)
 
     def _save_annotations_file(self) -> None:
-        self._refresh_eeg_fileset()
-        annotations_file = self._viewer_logic.save_annotations_file(self.data.eeg_fileset)
-        self._upsert_annotations_file(annotations_file)
-        self.annotations_file.set_dataclass(annotations_file)
+        save_task = self._viewer_logic.save_annotations_file(self.eeg_fileset)
+        save_task.add_done_callback(self._on_task_finished)
 
     def reset_state(self) -> None:
         super().reset_state()
@@ -107,7 +106,7 @@ class EGGAnnotatorLogic(BaseLogic[EEGAnnotatorState]):
     def set_ui(self, ui: EGGAnnotatorUI) -> None:
         self._viewer_logic.set_ui(ui.viewer_ui)
 
-        ui.annotation_saved.connect(self._save_annotations_file)
         ui.previous_clicked.connect(self.previous_clicked)
         ui.next_clicked.connect(self.next_clicked)
         ui.annotation_selected.connect(self._on_annotations_file_selected)
+        ui.annotation_saved.connect(self._save_annotations_file)
